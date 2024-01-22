8dd0d323fa0a403591c751423d8bf04f58b1358a
Rudra_Sharma <rudra.sharma0310@gmail.com>
2024-01-22 12:10:20 +0530

    This code has "For loop" practical for how to find even numbers

---
+++ EvenFinderWithForLoop.java
@@ -0,0 +1,14 @@
+public class EvenFinderWithForLoop
+ {
+    public static void main(String Args[])
+    {
+    int v = 200;
+    for(int i = 0;i<=v;i++)
+         {
+        if(i % 2 == 0)
+             {
+             System.out.println(i);
+             }
+         }
+    }
+ }
---
+++ EvenFinderWithWhileLoop.java
@@ -0,0 +1,16 @@
+public class EvenFinderWithWhileLoop
+ {
+    public static void main(String Args[])
+     { 
+        int v = 200;
+        int i = 0;
+         while(i <=v)
+         {
+         if(i % 2 == 0)
+         {
+           System.out.println(i);
+         }
+         i++;
+         }
+     }
+ }
